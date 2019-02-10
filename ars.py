95412e85b4a2d8413746613b2936acb540cabe89
Abhinav <abhinavkavuri@gmail.com>
2019-02-10 23:16:37 +0530

    Import libraries & set hyper parameters

---
+++ ars.py
@@ -0,0 +1,22 @@
+#Doc: 10-FEB-2019
+
+import os
+import numpy as np
+import gym
+from gym import wrappers
+import pybullet_envs
+
+#set Hyper-parameters
+
+class Hp():
+    
+    def __init__(self):
+        self.nb_steps = 1000
+        self.episode_length = 1000
+        self.learning_rate = 0.02
+        self.nb_directions = 16
+        self.nb_best_directions = 16
+        assert self.nb_best_directions <= self.nb_directions
+        self.noise = 0.03
+        self.seed = 1
+        self.env_name = ''
