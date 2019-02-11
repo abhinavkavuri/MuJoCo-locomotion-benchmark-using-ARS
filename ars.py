c17b27f850921197dae351cf4fc6e57c7d85e71d
Abhinav <abhinavkavuri@gmail.com>
2019-02-12 00:55:11 +0530

    Normalization of states
    Observer + Normalize

--- ars.py
+++ ars.py
@@ -6,7 +6,7 @@ import gym
 from gym import wrappers
 import pybullet_envs
 
-#set Hyper-parameters
+#set Hyper parameters
 
 class Hp():
     
@@ -20,3 +20,26 @@ class Hp():
         self.noise = 0.03
         self.seed = 1
         self.env_name = ''
+        
+#Normalization
+        
+
+class Normalizer():
+    
+    def __init__(self, nb_inputs):
+        self.n = np.zeros(nb_inputs)
+        self.mean = np.zeros(nb_inputs)
+        self.mean_diff = np.zeros(nb_inputs)
+        self.var = np.zeros(nb_inputs)
+    
+    def observe(self, x):
+        self.n += 1.
+        last_mean = self.mean.copy()
+        self.mean += (x - self.mean) / self.n
+        self.mean_diff += (x - last_mean) * (x - self.mean)
+        self.var = (self.mean_diff / self.n).clip(min = 1e-2)
+    
+    def normalize(self, inputs):
+        obs_mean = self.mean
+        obs_std = np.sqrt(self.var)
+        return (inputs - obs_mean) / obs_std
