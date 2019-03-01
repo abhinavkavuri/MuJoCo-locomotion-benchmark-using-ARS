e2071fc0da3c444cce2e3882c2e55437c2898e8b
Abhinav <abhinavkavuri@gmail.com>
2019-03-01 20:20:07 +0530

    Policy Update
    Evaluation and sampling of Deltas, Update Weights

--- ars.py
+++ ars.py
@@ -1,4 +1,4 @@
-#Doc: 10-FEB-2019
+#Doc: 1-MAR-2019
 
 import os
 import numpy as np
@@ -43,3 +43,30 @@ class Normalizer():
         obs_mean = self.mean
         obs_std = np.sqrt(self.var)
         return (inputs - obs_mean) / obs_std
+    
+#Policy
+        
+
+class Policy():
+    
+    def __init__(self, input_size, output_size):
+        self.theta = np.zeros((output_size, input_size))
+    
+    def evaluate(self, input, delta = None, direction = None):
+        if direction is None:
+            return self.theta.dot(input)
+        elif direction == "positive":
+            return (self.theta + hp.noise*delta).dot(input)
+        else:
+            return (self.theta - hp.noise*delta).dot(input)
+    
+    def sample_deltas(self):
+        return [np.random.randn(*self.theta.shape) for _ in range(hp.nb_directions)]
+    
+    def update(self, rollouts, sigma_r):
+        step = np.zeros(self.theta.shape)
+        for r_pos, r_neg, d in rollouts:
+            step += (r_pos - r_neg) * d
+        self.theta += hp.learning_rate / (hp.nb_best_directions * sigma_r) * step
+
+
